bd1929942e0b90c9bf1524718b16517f24250c4a
Salsabila Nur Adilah Al-ahmadi <salsabilanuradilahalahmadi@gmail.com>
2021-10-11 00:01:43 +0800

    Tugas 1
    Membuat kecepatan bola stabil

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    private Rigidbody2D rigidBody2D;
+    public float xInitialForce;
+    public float yInitialForce;
+    private Vector2 trajectoryOrigin;
+
+    void ResetBall()
+    {
+        transform.position = Vector2.zero;
+        rigidBody2D.velocity = Vector2.zero;
+    }
+    void PushBall()
+    {
+        float forcemagnitude = yInitialForce * yInitialForce + xInitialForce * xInitialForce;
+        float yRandomInitialForce = Random.Range(-yInitialForce, yInitialForce);
+        float xNewInitialForce = Mathf.Sqrt(forcemagnitude - yRandomInitialForce * yRandomInitialForce);
+        float randomDirection = Random.Range(0, 2);
+
+        if (randomDirection < 1.0f)
+        {
+            rigidBody2D.AddForce(new Vector2(-xNewInitialForce, yRandomInitialForce));
+        }
+        else
+        {
+            rigidBody2D.AddForce(new Vector2(xNewInitialForce, yRandomInitialForce));
+        }
+    }
+    void RestartGame()
+    {
+        ResetBall();
+        Invoke("PushBall", 2);
+
+    }
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        trajectoryOrigin = transform.position;
+        RestartGame();
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+}
---
+++ Assets/Scripts/SideWall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideWall : MonoBehaviour
+{
+    [SerializeField]
+    private gameManager gameManager;
+
+    public PlayerControl player;
+    private void OnTriggerEnter2D(Collider2D anotherCollider)
+    {
+        if(anotherCollider.name == "Ball")
+        {
+            player.IncrementScore();
+
+            if (player.Score < gameManager.maxScore)
+            {
+                anotherCollider.gameObject.SendMessage("RestartGame", 2.0f, SendMessageOptions.RequireReceiver);
+            }
+        }
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
